6397eb0bf8333b88d4f6b039051ff7c64095f355
Ted Landis <landis.edward@gmail.com>
2012-04-06 16:26:20 -0400

    added jars to classpath

--- ckdgcloudshout-Android/src/com/cloudshout/domain/smil/Head.java
+++ ckdgcloudshout-Android/src/com/cloudshout/domain/smil/Head.java
@@ -1,6 +1,30 @@
 package com.cloudshout.domain.smil;
 
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name="head")
 public class Head extends AbstractSMIL {
+	@XmlAttribute
 	private String id;
+	@XmlAttribute
 	private String title;
+	
+//	public Head(String id, String title) {
+//		this.id = id;
+//		this.title = title;
+//	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
