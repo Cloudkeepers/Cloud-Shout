e4d83619429957c34914b7def4be4c648a5672ad
Charles Henry <charles.henry0428@sentrysecuritysolutions.com>
2012-04-22 10:10:40 -0400

    Ready for hardcode of smil objects then testing

--- ckdgcloudshout-Android/src/com/cloudshout/FilteredControllerActivity.java
+++ ckdgcloudshout-Android/src/com/cloudshout/FilteredControllerActivity.java
@@ -1,9 +1,11 @@
 package com.cloudshout;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -15,41 +17,33 @@ import android.widget.Toast;
 
 import com.cloudshout.R;
 
+
 public class FilteredControllerActivity extends Activity {
-	
 	private ListView contentsListView;
 	private List<Object> contents = new ArrayList<Object>();
 	private TextView label;
 	
+	
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.filtered_controller);
-        
-        
         contentsListView = (ListView) findViewById(R.id.content);
         label = (TextView) findViewById(R.id.label);
         
-
 		String type = null;
 		Object source = null;
 		Bundle bundel = getIntent().getExtras(); 
-		
 		try{  
             type = (String) bundel.get("type");  
             source = (Object) bundel.get("source");  
         }catch(Exception e){
-    	}  
-
-        
-        
+    	}
 		label.setText(type.toUpperCase() + " from " + source.toString().toUpperCase());
-		
-		//load messages here
 		contents = new ArrayList<Object>();
 		
+		//TODO load contects from selected person, either on local device or the cloud
 		int total = (int) (Math.random()*15);
-		
 		if(type.equalsIgnoreCase("drafts")){
 			for (int i = 0; i < total; i++){
 				contents.add("Draft " + i);
@@ -68,27 +62,23 @@ public class FilteredControllerActivity extends Activity {
 					dayNight = "AM";
 				}
 				min = (int)(Math.random()* 59 ) + 1;
-				
 				contents.add(year + "/" + month + "/" + day + " " + hour + ":" + min + " " + dayNight + " EST");
 			}			
 		}
 		
 		
-		
-		//dynamic stuff here
 		contentsListView.setAdapter(new ArrayAdapter(this, R.layout.controller_item, contents));
-		//Add an onclicklisteners for messages
-		contentsListView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView parent, View view, int position, long id) {
+		contentsListView.setOnItemClickListener(new OnItemClickListener(){
+			public void onItemClick(AdapterView parent, View view, int position, long id){
 				String item = (String) contentsListView.getAdapter().getItem(position);
 				Toast.makeText(FilteredControllerActivity.this, "Open: " + item + ".", Toast.LENGTH_SHORT).show();
 				
-				/*Inbox items will be grouped by the "recieved from" contact
-				myIntent = new Intent(ControllerActivity.this, FilteredControllerActivity.class);
-				myIntent.putExtra("type","inbox");
-			    myIntent.putExtra("source",item);
-			    ControllerActivity.this.startActivity(myIntent);
-			    */
+				Intent myIntent = new Intent(FilteredControllerActivity.this, PlayerActivity.class);
+				//TODO Call parser here, get SMIL objects back
+				//myIntent.putExtra("type","inbox");
+			    //myIntent.putExtra("source",item);
+			    //release to player
+			    FilteredControllerActivity.this.startActivity(myIntent);
 				
 			}
 		});
---
+++ ckdgcloudshout-Android/src/com/cloudshout/PlayerActivity.java
@@ -0,0 +1,153 @@
+package com.cloudshout;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+public class PlayerActivity extends Activity {
+
+	private RelativeLayout root;
+	private int counter = 0;
+	private List<SMILMedia> mediaList;
+	private CountDownTimer timer;
+	private ProgressBar pbar;
+	
+	public void OnCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		root = new RelativeLayout(this);
+		
+		mediaList = new ArrayList<SMILMedia>();
+		
+		Intent receive = getIntent();
+		Bundle extras = receive.getExtras();
+		Serializable test = extras.getSerializable("SMILObjects");
+		
+		if(test instanceof SerializeSO) {
+			mediaList = ((SerializeSO)test).getList();
+			setMediaList(mediaList);
+		}
+		
+		if(mediaList.isEmpty() ) {
+			Toast empty = Toast.makeText(this.getBaseContext(), "No Message", 5000);
+			empty.show();
+		}
+		
+		setContentView(root);
+	}
+	
+	public void setMediaList(List<SMILMedia> mediaList) {
+		this.mediaList = mediaList;
+		loadMediaList();
+	}
+	
+	private void loadMediaList() {
+		for (SMILMedia media : this.mediaList){
+			if(media instanceof Text){
+				root.addView(((SMILVisual) media).createView(this), 
+						new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+								RelativeLayout.LayoutParams.WRAP_CONTENT));
+			}
+			else if(media instanceof Image) {
+				SMILVisual visual = (SMILVisual) media;
+				root.addView(visual.createView(this), new RelativeLayout.LayoutParams(visual.getRight(), visual.getBottom()));
+			}
+			else {
+				SMILVisual visual = (SMILVisual) media;
+				root.addView(visual.createView(this), new RelativeLayout.LayoutParams(visual.getRight(), visual.getBottom()));
+			}
+		}
+	}	
+	
+	private int getLength(List<SMILMedia> mediaList) {
+		int length = 0;
+		
+		for(SMILMedia smilMedia : mediaList) {
+			if (length <= smilMedia.getEnd()){
+				length = smilMedia.getEnd();
+			}
+		}
+		
+		return length;
+	}
+	
+	private void setUpProgressBar() {
+		pbar = new ProgressBar(this, null, android.R.attr.progressBarStyleHorizontal);
+		LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+				ViewGroup.LayoutParams.WRAP_CONTENT);
+		
+		root.addView(pbar, layoutParams);
+		pbar.setPadding(0,  485, 0, 0);
+		pbar.setMax(getLength(mediaList));
+	}
+	
+	private void playMessage(int length, int cntr) {
+		setUpProgressBar();
+		pbar.setVisibility(View.VISIBLE);
+		timer = new CountDownTimer(((long)(length - cntr + 1) * 1000), 1000){
+			public void onTick(long mUF) {
+				pbar.setProgress(counter);
+				beginEnd(counter);
+				counter++;
+			}
+			
+			public void onFinish() {
+				pbar.setProgress(counter);
+				stopMessage();
+			}
+			
+		};
+		
+		timer.start();
+	}
+	
+	private void pauseMessage() {
+		timer.cancel();
+		for(SMILMedia smilMedia : mediaList) {
+			smilMedia.pauseSO();
+		}
+		// mDialog lines commented out until I double check design with team
+		// these will probably be replaced with Android buttons
+		//mDialog("Paused:", "Resume", "Close", 4);
+	}
+	
+	private void resumeMessage() {
+		for(SMILMedia smilMedia : mediaList) {
+			smilMedia.resumeSO();
+		}
+		
+		playMessage(getLength(mediaList), counter);
+	}
+	
+	private void stopMessage() {
+		counter = 0;
+		for(SMILMedia smilMedia : mediaList) {
+			smilMedia.endSO();
+		}
+		// mDialog lines commented out until I double check design with team
+		// these will probably be replaced with Android buttons
+		//mDialog("Message Stopped:", "Replay", "Close", 4);
+	}
+	
+	private void beginEnd(int counter) {
+		for(SMILMedia smilMedia : mediaList) {
+			if(counter == smilMedia.getBegin()){
+				smilMedia.startSO();
+			}
+			if(counter == smilMedia.getEnd()){
+				smilMedia.endSO();
+			}
+		}
+	}
+}
